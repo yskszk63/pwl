bd6c78a1c76f52c78f9936fb5e6ca408f74252ff
yskszk63 <yskszk63@gamil.com>
2019-04-23 00:37:22 +0900

    opt segments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{crate_name, crate_version, App, Arg};
+use clap::{crate_name, crate_version, App, Arg, arg_enum, values_t_or_exit};
 
 use crate::powerline::Powerline;
 use crate::segments::Segments;
@@ -11,30 +11,60 @@ mod segments;
 mod shell;
 mod theme;
 
+arg_enum! {
+    pub enum CliSegments {
+        Virtualenv,
+        Username,
+        Hostname,
+        Ssh,
+        Cwd,
+        Git,
+        Jobs,
+        Root,
+    }
+}
+
+impl CliSegments {
+    pub fn into_segments(self) -> Segments {
+        match self {
+            CliSegments::Root => Segments::Root,
+            CliSegments::Cwd => Segments::Cwd,
+            CliSegments::Jobs => Segments::Jobs,
+            CliSegments::Virtualenv => Segments::Virtualenv,
+            CliSegments::Username => Segments::Username,
+            CliSegments::Hostname => Segments::Hostname,
+            CliSegments::Ssh => Segments::Ssh,
+            CliSegments::Git => Segments::Git,
+        }
+    }
+}
+
 fn main() {
+    let x = CliSegments::variants().join(",");
+
     let matches = App::new(crate_name!())
         .version(crate_version!())
         .arg(Arg::with_name("RC"))
+        .arg(Arg::with_name("segments")
+             .short("s")
+             .possible_values(&CliSegments::variants())
+             .value_name("SEGMENTS")
+             .value_delimiter(",")
+             .case_insensitive(true)
+             .default_value(&x))
         .get_matches();
 
     let rc = matches
         .value_of("RC")
         .map(|rc| rc.parse::<i32>().unwrap_or(-1));
+    let segments = values_t_or_exit!(matches, "segments", CliSegments);
+    let segments = segments.into_iter().map(move |s| s.into_segments()).collect::<Vec<_>>();
     let theme = Theme::default();
     let shell = Shell::Bash;
 
     let output = std::io::stdout();
     let mut output = std::io::BufWriter::new(output.lock());
     let mut pwl = Powerline::new(rc, theme, shell, &mut output);
-    let segments = [
-        Segments::Virtualenv,
-        Segments::Username,
-        Segments::Hostname,
-        Segments::Ssh,
-        Segments::Cwd,
-        Segments::Git,
-        Segments::Jobs,
-        Segments::Root,
-    ];
+
     pwl.draw(&segments).unwrap();
 }
